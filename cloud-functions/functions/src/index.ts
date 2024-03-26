64e15ed9ad4aa3e195887800dc867058fec9411a
Nethmi-24 <batawalann.20@uom.lk>
2024-03-26 10:19:45 +0530

    update function added with errors

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,8 +1,7 @@
 import * as v2 from "firebase-functions/v2";
 const admin = require('firebase-admin');
-
 admin.initializeApp();
-
+const functions = require('firebase-functions');
 
 const firestore = admin.firestore();
 
@@ -33,3 +32,26 @@ exports.validateName=v2.https.onRequest({cors:true},(req,res:any)=>{
       }
    res.json({ isValid: true });
 })
+
+
+exports.updateUser = functions.firestore
+  .document('Users/{userId}')
+  .onUpdate(async (change:any, context:any) => { 
+    const newValue = change.after.data();
+    const previousValue = change.before.data();
+
+    if (newValue.name !== previousValue.name) {
+        const userId = context.params.userId;
+        const newName = newValue.name;
+        
+        try {
+            await firestore.collection('Users').doc(userId).update({
+              name: newName
+            });
+
+          console.log('Name updated successfully:', newName);
+        } catch (error) {
+          console.error('Error updating name:', error);
+        }
+      }
+  });
