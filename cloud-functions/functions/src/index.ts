2d13493ddea46bd1aa21e2c326aef9508a7f5036
Nethmi-24 <batawalann.20@uom.lk>
2024-03-27 14:35:49 +0530

    update func in version2

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,100 +1,129 @@
 import * as v2 from "firebase-functions/v2";
-import * as v1 from "firebase-functions/v1";
-import { onDocumentCreated } from 'firebase-functions/v2/firestore';
-const admin = require('firebase-admin');
+import {
+  onDocumentCreated,
+  onDocumentUpdated,
+  Change,
+  FirestoreEvent,
+} from "firebase-functions/v2/firestore";
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 const firestore = admin.firestore();
 
-
-export const createTask = v2.https.onRequest({cors:true},(request,response)=>{
-   
+export const createTask = v2.https.onRequest(
+  { cors: true },
+  (request, response) => {
     const data = request.body;
-    if(data.date.length===0 || data.task_description.length===undefined || data.task_status===undefined || data.userID.length===undefined){
-        return response.send(400).json({
-            message:"Bad Request !"
-        })
-    };
-    return firestore.collection('Tasks').add(data).then(()=>{return response.send(200).json({messsage:"task created succefully"})}).catch((err:any) =>{return response.status(500).json({message:"Internal server error"})})
+    if (
+      data.date.length === 0 ||
+      data.task_description.length === undefined ||
+      data.task_status === undefined ||
+      data.userID.length === undefined
+    ) {
+      return response.send(400).json({
+        message: "Bad Request !",
+      });
+    }
+    return firestore
+      .collection("Tasks")
+      .add(data)
+      .then(() => {
+        return response.send(200).json({ messsage: "task created succefully" });
+      })
+      .catch((err: any) => {
+        return response.status(500).json({ message: "Internal server error" });
+      });
+  }
+);
 
-})
+exports.validateName = v2.https.onRequest({ cors: true }, (req, res: any) => {
+  const name: any = req.query.name;
+  if (/\d/.test(name)) {
+    return res.status(400).json({ error: "Name cannot contain numbers" });
+  }
+  res.json({ isValid: true });
+});
 
+exports.updateUser = onDocumentUpdated("/Users/{userId}", async(event) => {
+  if (!event.data) {
+    throw "";
+  }
+  const newValue = event.data.after.data();
+  const previousValue = event.data.before.data();
 
-exports.validateName=v2.https.onRequest({cors:true},(req,res:any)=>{
-    const name:any=req.query.name;
-    if (/\d/.test(name)) {
-        return res.status(400).json({ error: 'Name cannot contain numbers' });
-      }
-   res.json({ isValid: true });
-})
+  if (
+    newValue.name &&
+    previousValue.name &&
+    newValue.name !== previousValue.name
+  ) {
+    const userId = event.params.userId;
+    const newName = newValue.name;
 
-// exports.updateUser = v1.firestore
-// .document('/Users/{userId}')
-// .onUpdate(async (change, context) => { 
-//   const newValue = change.after.data();
-//   const previousValue = change.before.data();
+    try {
+      await firestore.collection("Users").doc(userId).update({
+        name: newName,
+      });
+      console.log("Name updated successfully:", newName);
+    } catch (error) {
+      console.error("Error updating name:", error);
+    }
+  }
+});
 
-//   if (newValue.name && previousValue.name && newValue.name !== previousValue.name) { 
-//       const userId = context.params.userId;
-//       const newName = newValue.name;
-      
-//       try {
-//           await firestore.collection('Users').doc(userId).update({
-//             name: newName
-//           });
-//         console.log('Name updated successfully:', newName);
-//       } catch (error) {
-//         console.error('Error updating name:', error);
-//       }
-//     }
-//   });
+type taskData = {
+  date: Date;
+  task_description: string;
+  task_status: boolean;
+  userID: string;
+};
 
-
-type taskData = {date:Date,task_description:string,task_status:boolean,userID:string}
-
-
-  
-
-  export const newTask = onDocumentCreated({document:"Tasks/{taskId}"},async event =>{
-    
+export const newTask = onDocumentCreated(
+  { document: "Tasks/{taskId}" },
+  async (event) => {
     const regex = /\d/;
-    if(!event.data){
-      throw ""
+    if (!event.data) {
+      throw "";
     }
     let data = event.data.data() as taskData;
     // console.log("new task data",event.data);
     let newDescription = "";
-  
-    if(regex.test(data.task_description)){
-      
+
+    if (regex.test(data.task_description)) {
       newDescription = "without numbers";
-      data = {...data ,task_description :newDescription};
+      data = { ...data, task_description: newDescription };
       // console.log("new description created")
-      
     }
-    try{
-      const result =  await event.data.ref.set({...data},{merge:true});
-   console.log("result",result);
-
-    }catch(err){
-      console.log("task updat error",err)
+    try {
+      const result = await event.data.ref.set({ ...data }, { merge: true });
+      console.log("result", result);
+    } catch (err) {
+      console.log("task updat error", err);
     }
-    
-   
-  //  return result;
-  })
 
-  
+    //  return result;
+  }
+);
 
+exports.createNewUser = onDocumentCreated(
+  { document: "/Users/{userid}" },
+  async (event) => {
+    if (!event.data) {
+      throw "err";
+    }
+    let userToCreate = event.data.data();
+    const capitalizedName = userToCreate.name
+      ? userToCreate.name.charAt(0).toUpperCase() + userToCreate.name.slice(1)
+      : "";
 
-
-
-exports.createNewUser=v1.firestore.document('/Users/{userid}').onCreate(snapshot =>{
-  let userToCreate = snapshot.data();
-  const capitalizedName = userToCreate.name ? userToCreate.name.charAt(0).toUpperCase() + userToCreate.name.slice(1) : '';
-
-  userToCreate = {...userToCreate,name:capitalizedName};
-
-  return snapshot.ref.set({...userToCreate},{merge:true});
-})
-
+    userToCreate = { ...userToCreate, name: capitalizedName };
+    try {
+      const result = await event.data.ref.set(
+        { ...userToCreate },
+        { merge: true }
+      );
+      console.log("result", result);
+    } catch (err) {
+      console.log("task updat error", err);
+    }
+  }
+);
